e26c197a243587cb9351d38f1deb3ed0ce0f43d6
Ryota Tominaga <ryota@Ryota-MacBook-Pro.local>
2017-11-13 01:04:59 +0900

    11/12

--- Lesson12/Montyhall.java
+++ Lesson12/Montyhall.java
@@ -18,47 +18,46 @@ class Montyhall{
       String str = br.readLine();
       int n = Integer.parseInt(str);
 
-        //最初に選択するドアを決める
-        for(int i=0; i<n; i++){
-          int ran = rnd1.nextInt(3);
-          if(ran == 0){
-            sld = 0;
-          }else if(ran == 1){
-            sld = 1;
-          }else{
-            sld = 2;
-          }
-          //正解のドアを決める
-          for(int s=0; s<n; s++){
-            rand = rnd2.nextInt(3);
-            if(rand == 0){
-              cord = 0;
-            }else if(rand == 1){
-              cord = 1;
-            }else{
-              cord = 2;
-            }
-          }
-          //最初から正解のドアを選んできた場合
-          if(sld == cord){
-            shwd = (sld + (rnd3.nextInt(2)+1))%3;
-            cntNC++;
-          }else if((sld + cord) == 1){
-            shwd = 2;
-            cntAC++;
-          }else if((sld + cord) == 2){
-            shwd = 1;
-            cntAC++;
-          }else if((sld + cord) == 3){
-            shwd = 0;
-            cntAC++;
-          }
-
+      //最初に選択するドアを決める
+      for(int i=0; i<n; i++) {
+        int ran = rnd1.nextInt(3);
+        if(ran == 0) {
+          sld = 0;
+        } else if(ran == 1) {
+          sld = 1;
+        } else {
+          sld = 2;
         }
+        //正解のドアを決める
+        for(int s=0; s<n; s++) {
+          rand = rnd2.nextInt(3);
+          if(rand == 0) {
+            cord = 0;
+          } else if(rand == 1) {
+            cord = 1;
+          } else {
+            cord = 2;
+          }
+        }
+        //最初から正解のドアを選んできた場合
+        if(sld == cord){
+          shwd = (sld + (rnd3.nextInt(2)+1))%3;
+          cntNC++;
+        } else if((sld + cord) == 1) {
+          shwd = 2;
+          cntAC++;
+        } else if((sld + cord) == 2) {
+          shwd = 1;
+          cntAC++;
+        } else if((sld + cord) == 3) {
+          shwd = 0;
+          cntAC++;
+        }
+      }
 
         System.out.println("常に変更したときの正解数" + cntAC);
         System.out.println("変更しないときの正解数" + cntNC);
-        
+
       }catch(IOException e) {
         System.err.println(e.getMessage());
       }
