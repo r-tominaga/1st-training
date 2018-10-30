f0700e172df096cd2047328275d05756f3e106f8
r-tominaga <r-tominaga@ttc-net.co.jp>
2018-10-30 09:58:30 +0900

    削除

--- tbc-lite/tbcLite/app/models/history.rb
+++
@@ -1,3 +0,0 @@
-class History < ApplicationRecord
-  attr_accessor :from, :to, :amount, :comment
-end
--- tbc-lite/tbcLite/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  def password_confirmation=(val)
-    if val.present?
-      self.password_digest = BCrypt::Password.create(val)
-    end
-    @password_digest = val
-  end
-end
